893073ee4a967facf8a7b6fc874b3229190980ce
sydqwy <1398698913@qq.com>
2019-05-02 02:36:23 +0800

    修改与添加，5.1

---
+++ data/post-data.js
@@ -0,0 +1,53 @@
+var local_data = [{
+    time: '2018-07-21',
+    title: '美丽的时光',
+    img: '/images/1.jpg',
+    bjimg: '/images/2.jpg',
+    plimg: '/images/3.jpg',
+    dzimg: '/images/4.jpg',
+    content: '世界的终结者们,就是如此的忧伤',
+    dz: '35',
+    pl: '31',
+    pid:0
+  },
+  {
+    time: '2018-07-21',
+    title: '悠闲的时代',
+    img: '/images/1.jpg',
+    bjimg: '/images/2.jpg',
+    plimg: '/images/3.jpg',
+    dzimg: '/images/4.jpg',
+    content: '闲暇的时光真是悠闲',
+    dz: '353',
+    pl: '21',
+    pid:1
+  },
+   {
+    time: '2018-08-21',
+    title: '时代',
+    img: '/images/1.jpg',
+    bjimg: '/images/2.jpg',
+    plimg: '/images/3.jpg',
+    dzimg: '/images/4.jpg',
+    content: '闲暇的时光真是悠闲',
+    dz: '13',
+    pl: '20',
+    pid:2
+  },
+   {
+    time: '2018-07-30',
+    title: '记忆',
+    img: '/images/1.jpg',
+    bjimg: '/images/2.jpg',
+    plimg: '/images/3.jpg',
+    dzimg: '/images/4.jpg',
+    content: '闲暇的时光真是悠闲',
+    dz: '22',
+    pl: '21',
+    pid:3
+  }
+]
+
+module.exports = {
+  dataList : local_data
+}
--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,66 +1,73 @@
-// pages/posts/posts.js
+var postList = require('../../data/post-data.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
   },
 
+  onPostTap:function(event){
+    var pid=event.currentTarget.dataset.id;
+    wx.navigateTo({
+      url:"./post-detail/post-detail",
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+     this.setData({postKey:postList.dataList})
+    //this.data.postKey = postList.dataList
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    console.log('页面显示')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
+  onHide: function() {
+    console.log('页面隐藏')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
+  onUnload: function() {
+    console.log('页面关闭')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
+  onPullDownRefresh: function() {
+    console.log('用户下拉')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
+  onReachBottom: function() {
+    console.log('页面触底')
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function() {
+    console.log('页面分享')
   }
 })
